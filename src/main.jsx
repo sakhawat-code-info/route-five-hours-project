d02eb650e6c08df1f8e7205ac37c5fb6647b4309
sakhawat-code-info <sakhawat.code.info@gmail.com>
2024-03-24 09:01:25 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './index.css'
+import ErrorPage from './pages/ErrorPage';
+import Root from './LayOut/Root';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <div>outlet body</div>,
+      },
+    ],
+  },
+]);
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
